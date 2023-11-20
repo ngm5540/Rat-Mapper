7531d5731b797ccde30c9ca8f79c2c830333c491
natejank <njj3397@rit.edu>
2023-11-19 20:21:32 -0500

    add background colors

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import { useLocation } from "preact-iso";
+import "./Header.css";
 
 export function Header() {
     const { url } = useLocation();
 
     return (
         <header>
-            <div class="flex items-center mx-8">
+            <div class="flex items-center p-4 bg-indigo-500 text-white">
                 <div class="w-1/3 flex flex-row items-center space-x-8">
                     <img
                         src="/public/spin.gif"
@@ -13,20 +14,12 @@ export function Header() {
                         style="max-width:100px"
                         class="rounded-full"
                     />
-                    <button class="w-36 h-12 rounded-md hover:bg-green-900">
-                        Rat mapper 🐀
-                    </button>
+                    <button class="w-36 nav_button">Rat mapper 🐀</button>
                 </div>
-                <div class="ml-auto flex flex-row items-center space-x-16">
-                    <button class="w-20 h-12 rounded-md hover:bg-green-900">
-                        Home
-                    </button>
-                    <button class="w-20 h-12 rounded-md hover:bg-green-900">
-                        Breed
-                    </button>
-                    <button class="w-28 h-12 rounded-md hover:bg-green-900">
-                        Family Tree
-                    </button>
+                <div class="ml-auto flex flex-row items-center space-x-8">
+                    <button class="w-20 nav_button">Home</button>
+                    <button class="w-20 nav_button">Breed</button>
+                    <button class="w-28 nav_button">Family Tree</button>
                 </div>
             </div>
         </header>
