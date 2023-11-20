92a55f2f3538202210ccf80fd04bbf03a8168b53
natejank <njj3397@rit.edu>
2023-11-19 20:07:18 -0500

    make a sensible looking header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,19 +1,34 @@
-import { useLocation } from 'preact-iso';
+import { useLocation } from "preact-iso";
 
 export function Header() {
-	const { url } = useLocation();
+    const { url } = useLocation();
 
-	return (
-		<header>
-			<nav>
-                <img src="/public/spin.gif" alt="Rat spinning horizontally" style="max-width:100px"/>
-				<p> Rat Mapper </p>
-			</nav>
-			<nav>
-				<a href="/" class={url == '/' && 'active'}>
-					Home
-				</a>
-			</nav>
-		</header>
-	);
+    return (
+        <header>
+            <div class="flex items-center mx-8">
+                <div class="w-1/3 flex flex-row items-center space-x-8">
+                    <img
+                        src="/public/spin.gif"
+                        alt="Rat spinning horizontally"
+                        style="max-width:100px"
+                        class="rounded-full"
+                    />
+                    <button class="w-36 h-12 rounded-md hover:bg-green-900">
+                        Rat mapper 🐀
+                    </button>
+                </div>
+                <div class="ml-auto flex flex-row items-center space-x-16">
+                    <button class="w-20 h-12 rounded-md hover:bg-green-900">
+                        Home
+                    </button>
+                    <button class="w-20 h-12 rounded-md hover:bg-green-900">
+                        Breed
+                    </button>
+                    <button class="w-28 h-12 rounded-md hover:bg-green-900">
+                        Family Tree
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
 }
