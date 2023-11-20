d069065ba0b1402da876a93adecd65a188db19b7
natejank <njj3397@rit.edu>
2023-11-19 22:22:12 -0500

    make the design a bit more responsive

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,20 +6,23 @@ export function Header() {
 
     return (
         <header>
-            <div class="flex items-center p-4 bg-indigo-500 text-white">
-                <div class="w-1/3 flex flex-row items-center space-x-8">
+            <div class="flex items-center sm:px-2 md:p-4 bg-indigo-500 text-white">
+                <div class="md:w-1/3 flex flex-row items-center space-x-2 md:space-x-8">
                     <img
                         src="/public/spin.gif"
                         alt="Rat spinning horizontally"
                         style="max-width:100px"
-                        class="rounded-full"
+                        class="rounded-full hidden md:block"
                     />
-                    <button class="w-36 nav_button text-lg">Rat Mapper</button>
+                    <button class="md:w-36 nav_button md:text-lg">
+                        Rat Mapper
+                    </button>
                 </div>
-                <div class="ml-auto flex flex-row items-center space-x-8">
-                    <button class="w-20 nav_button">Home</button>
-                    <button class="w-20 nav_button">Breed</button>
-                    <button class="w-28 nav_button">Family Tree</button>
+                <div class="overflow-auto ml-auto flex flex-row items-center space-x-4">
+                    <button class="md:w-20 nav_button">Home</button>
+                    <button class="md:w-24 nav_button">About</button>
+                    <button class="md:w-20 nav_button">Breed</button>
+                    <button class="md:w-28 nav_button">Family Tree</button>
                 </div>
             </div>
         </header>
