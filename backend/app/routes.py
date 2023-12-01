d8d7361c7c04bed606207f5640b6286db777cc06
Nate Mount <ngmount2021@gmail.com>
2023-11-30 20:04:34 -0500

    Updated routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,17 +1,20 @@
 # [Routes - App]
 # Authors: Nate Mount (ngm5540)
 
-from flask import request
+from flask import request, jsonify
 
 from app import app
 from app.models.brokers import RatHook
 
 @app.route('/get/<rat_id>')
 def get_rat(rat_id:int):
+    return jsonify(RatHook.get(id=int(rat_id)))
 
-    data = RatHook.get(id=int(rat_id))
 
 @app.route('/save')
 def save_rat():
     if request.get_json():
-        RatHook.new(**request.get_json())
+        RatHook.insert(**request.get_json())
+        return jsonify({'message' : 'Success'}), 200
+    else:
+        return jsonify({'message' : 'Json Required!!'}), 401
