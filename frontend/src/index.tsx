6d981147a326c3d67f2581f135488fd907b6ce88
natejank <njj3397@rit.edu>
2023-12-11 14:45:32 -0500

    remove unused pages

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,7 @@
 import { render } from "preact";
 import { LocationProvider, Router, Route } from "preact-iso";
 import { Header } from "./components/Header";
-import { Analyze } from "./pages/Analyze/Analyze";
 import { NotFound } from "./pages/_404";
-import { Test } from "./pages/Test/Test";
 import { About } from "./pages/About/About";
 import Home from "./pages/Home/Home";
 import { Breed } from "./pages/Breed/Breed";
@@ -16,11 +14,9 @@ export function App() {
             <main>
                 <Router>
                     <Route component={Home} path="/" />
-                    <Analyze path="/analyze/:dna" />
                     <Route component={Breed} path="/breed" />
                     <Route default component={NotFound} />
                     <Route component={Tree} path="/tree" />
-                    <Test path="/test" />
                     <Route component={About} path="/about" />
                 </Router>
             </main>
