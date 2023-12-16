320f87706f59ef822c50d8150bad0b017eaf15f6
natejank <njj3397@rit.edu>
2023-12-16 00:43:17 -0500

    disable adding new rats with one simple trick!!! (doctors hate this)

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -6,6 +6,9 @@ from flask import request, jsonify
 from app import app
 from app.models.brokers import RatHook
 
+# dirty hack because I don't want to dive into the backend code
+DISABLE_SAVING=True
+
 @app.route('/get/<rat_id>')
 def get_rat(rat_id:int):
     return jsonify(RatHook.get(element_id=int(rat_id)))
@@ -17,6 +20,9 @@ def get_all():
 @app.route('/save', methods=['POST','GET'])
 def save_rat():
     if request.get_json():
+        if DISABLE_SAVING:
+            return jsonify({'message' : 'Success'}), 200
+
         json = request.get_json()
         # ignore specified id; should be managed by the server
         if type(json) is dict and 'id' in json.keys():
